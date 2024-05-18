e5dc073a83aabbfcf2361258ab0669343d6d17ec
Rawan-Alnaggary <148374317+Rawan-Alnaggary@users.noreply.github.com>
2024-05-18 17:26:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "Heap_tree.h"
+#include "AVL.h"
 
 using namespace std;
 
 int main() {
-    Trees<Item> *test = new Heap_tree<Item>(false);
+    Trees<Item> *test = new AVLTree<Item>;
     test->Add_item(Item("cantaloupe", "fruit", 20));
     test->Add_item(Item("doulab", "feature", 20));
     test->Add_item(Item("banana", "fruit", 75));
@@ -11,9 +12,9 @@ int main() {
     test->Add_item(Item("fish", "animal", 75));
     test->Add_item(Item("apple", "fruit", 10));
     test->Add_item(Item("giraffe", "fruit", 10));
+//    test->Delete_item(1);
     test->Display_sort_by_name_descending();
-    test->Delete_item(1);
-    cout<<"\n-----------------------------\n\n";
+    cout<<"\n-------------------\n\n";
     test->Display_normal();
     delete test;
 }
